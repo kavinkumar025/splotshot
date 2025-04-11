8ac5228f3a5277694243e6474fa9a5e56cafbe34
kavinkumar <kavinkumar025@gmail.com>
2025-04-11 18:50:57 +0530

    Updated file

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyCVLN6s70uLbXI7px8ishct32JDXtGGLq4",
+        authDomain: "social-media-995d9.firebaseapp.com",
+        projectId: "social-media-995d9",
+        storageBucket: "social-media-995d9.firebasestorage.app",
+        messagingSenderId: "177622635042",
+        appId: "1:177622635042:web:6619ca99751856fe0e307b",
+        measurementId: "G-JXS8720E94"
+    }
+};
